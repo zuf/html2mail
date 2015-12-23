3f1d2805e56e3248c846acdd520f75b3e275ec41
Egor Vakhromtsev <e.vakhromtsev@mediacrat.com>
2015-12-23 18:26:14 +0300

    Do not process remote images

--- lib/html2mail/mailer2.rb
+++ lib/html2mail/mailer2.rb
@@ -3,38 +3,32 @@ require "action_mailer/base"
 require 'premailer'
 
 class Mailer2 < ActionMailer::Base
-
-  def prepare_html(html_file)
-    Premailer.new(html_file, :warn_level => Premailer::Warnings::SAFE).to_inline_css
-  end
-
   def mail2(html_file, email, subject, from=nil)
     premailer = Premailer.new(html_file, :warn_level => Premailer::Warnings::SAFE)
-    # STDOUT.puts premailer.to_inline_css
     doc = Nokogiri::HTML(premailer.to_inline_css)
-    # doc = File.open(html_file) { |f| Nokogiri::XML(f) }
 
     doc.css('img').each do |img|
       path = img['src']
-      STDERR.puts "Process img: #{path}"
-      attachments.inline[File.basename(path)] = File.read(File.expand_path(path, File.dirname(html_file)))
-      img['src'] = attachments.inline[File.basename(path)].url
+      filesystem_path = File.expand_path(path, File.dirname(html_file))
+      if File.readable?(filesystem_path)
+        STDERR.puts "Process img: #{path}"
+        attachments.inline[File.basename(path)] = File.read(filesystem_path)
+        img['src'] = attachments.inline[File.basename(path)].url
+      end
     end
 
     head_title = doc.css('head title')
-    subject ||= head_title.text  if head_title.present?
-    subject ||= File.basename(html_file)
+    subject ||= head_title.text if head_title.present?
+    subject ||= File.basename(html_file, File.extname(html_file))
 
     html = doc.to_s.html_safe
 
     from ||= email
 
-    m = mail from: from, to: email, subject: subject do |format|
+    mail from: from, to: email, subject: subject do |format|
       format.text { render plain: premailer.to_plain_text }
       format.html { render html: html }
     end
-
-    return m
   end
 end
 
